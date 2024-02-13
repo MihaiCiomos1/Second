0ea9d92dec1c7082d2349188fc19e0587063e549
Horia Mihai Ciomos <Mihai.Ciomos@endava.com>
2024-02-13 17:45:17 +0200

    another small fix

--- src/test/java/Tests/WebTableTest.java
+++ src/test/java/Tests/WebTableTest.java
@@ -28,32 +28,32 @@ public class WebTableTest  extends ShareData {
         webTablePage = new WebTablePage(getDriver());
         homePage.goToDesiredMenu("Elements");
         commonPage.goToDesiredSubMenu("Web Tables");
-        String firstN = "Cristi";
-        String lastN= "Pop";
-        String emalAdr="test@test.com";
-        String ageNr="25";
-        String salaryVal="1000";
-        String departmentName="Testing";
-        List<String> listaFinala= new ArrayList<>();
-        listaFinala.add(firstN);
-        listaFinala.add(lastN);
-        listaFinala.add(emalAdr);
-        listaFinala.add(ageNr);
-        listaFinala.add(departmentName);
-        WebElement tabel = getDriver().findElement(By.xpath("//div[@class='rt-tbody']/div/div[@class='rt-tr -even' or @class='rt-tr -odd']"));
-        List<String> listaInitiala= new ArrayList<String>();
-        List<WebElement> celule=tabel.findElements(By.tagName("td"));
-        for(int j=0; j<celule.size(); j++)
-        {
-            listaInitiala.add(celule.get(j).getText());
-        }
-        for(int i=0;i<listaFinala.size();i++) {
-            if(listaInitiala.get(i).contains(listaFinala.get(i))) {
-                System.out.println("Search function verified");
-            }else {
-                System.out.println("Search function verification failed"+listaInitiala.get(i));
-            }
-        }
+//        String firstN = "Cristi";
+//        String lastN= "Pop";
+//        String emalAdr="test@test.com";
+//        String ageNr="25";
+//        String salaryVal="1000";
+//        String departmentName="Testing";
+//        List<String> listaFinala= new ArrayList<>();
+//        listaFinala.add(firstN);
+//        listaFinala.add(lastN);
+//        listaFinala.add(emalAdr);
+//        listaFinala.add(ageNr);
+//        listaFinala.add(departmentName);
+//        WebElement tabel = getDriver().findElement(By.xpath("//div[@class='rt-tbody']/div/div[@class='rt-tr -even' or @class='rt-tr -odd']"));
+//        List<String> listaInitiala= new ArrayList<String>();
+//        List<WebElement> celule=tabel.findElements(By.tagName("td"));
+//        for(int j=0; j<celule.size(); j++)
+//        {
+//            listaInitiala.add(celule.get(j).getText());
+//        }
+//        for(int i=0;i<listaFinala.size();i++) {
+//            if(listaInitiala.get(i).contains(listaFinala.get(i))) {
+//                System.out.println("Search function verified");
+//            }else {
+//                System.out.println("Search function verification failed"+listaInitiala.get(i));
+//            }
+//        }
         //div[@class='rt-tbody']/div/div[@class='rt-tr -even']");
 //        List<WebElement> tableElements = driver.findElements(By.xpath(
 //            "//div[@class='rt-tbody']/div/div[@class='rt-tr -even' or @class='rt-tr -odd']"));
